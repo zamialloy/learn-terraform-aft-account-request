05486a51b12165b5803147d2f5b6137a82ad47b7
Zami Talukder <zami@alloy.co>
2022-12-15 01:44:03 -0500

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "zami+sandbox-aft@alloy.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "zami@alloy.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
